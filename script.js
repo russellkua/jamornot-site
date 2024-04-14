2f08350544e4d1eb47b6fe0c5dffc3815195b389
Russell <34262970+russellkua@users.noreply.github.com>
2024-04-14 13:56:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var currentYear = new Date().getFullYear();
-    document.getElementById("copyright").textContent = "Copyright " + currentYear;
+    // Load environment variables from .env file
+    require('dotenv').config();
+
+    // Initialize map
+    mapboxgl.accessToken = 'pk.eyJ1IjoicnVzc2VsbGt1YSIsImEiOiJjbHV3dzRwYzQwaHczMmlwYmZvOGYyd2lvIn0.nmwaGUMkYE4pxsAlI6kKbw'; // Access token from environment variable
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/russellkua/cluz0xrqb004o01phewerdwcc', // Style URL from environment variable
+        center: [103.8198, 1.3521], // Singapore coordinates
+        zoom: 12
+    });
+
+    // Add markers or other map functionalities here
 });
